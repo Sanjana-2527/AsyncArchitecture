8d38f6059cad26b3c826e6fbb5db6d9492445e87
Sanjana-2527 <97084077+Sanjana-2527@users.noreply.github.com>
2022-02-07 20:02:02 +0530

    Add files via upload

---
+++ AwsSQSConfig.java
@@ -0,0 +1,31 @@
+package com.sanjana.aws.sqs.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class AwsSQSConfig {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String awsAccessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String awsSecretKey;
+
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate(){
+        return new QueueMessagingTemplate(amazonSQSAsync());
+    }
+    @Primary
+    @Bean
+    public AmazonSQSAsync amazonSQSAsync(){
+        return AmazonSQSAsyncClientBuilder.standard().withRegion(Regions.AP_SOUTH_1)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)))
+                .build();
+    }
+}
