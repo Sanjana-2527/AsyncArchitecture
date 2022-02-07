b08986a30be644b388a267e77c74683c751df92c
Sanjana-2527 <97084077+Sanjana-2527@users.noreply.github.com>
2022-02-07 21:30:14 +0530

    Update AWSSNSConfig.java

--- AWSSNSConfig.java
+++ AWSSNSConfig.java
@@ -17,7 +17,7 @@ public class AWSSNSConfig {
     @Primary
     @Bean
     public AmazonSNSClient getSnsClient() {
-        return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.AP_SOUTH_1)
+        return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.US_EAST_1)
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY,
                         SECRET_KEY)))
                 .build();
